3f78c9c1ef8e38fec4ef5815ab7b01d5e460d6f3
User <user@Users-MacBook-Pro.local>
2020-03-16 10:20:14 +0800

    enhancement

--- src/modules/Reference/Reference.component.tsx
+++ src/modules/Reference/Reference.component.tsx
@@ -3,7 +3,7 @@ import { Grid, Typography } from '@material-ui/core';
 
 const ReferenceComponent: React.FC<{}> = () => {
     return <Grid container direction="column" justify="flex-start" spacing={2}>
-        <Grid item><Typography variant="h5"> Intro </Typography></Grid>
+        <Grid item><Typography variant="h5"> Reference </Typography></Grid>
         <Grid item>This is an example description content data</Grid>
     </Grid>;
 }
